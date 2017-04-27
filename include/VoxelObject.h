023197494358d0cc4e330985514c310945e72973
Gunnar Wambaugh <gwambaugh@hpcvis12.cse.unr.edu>
2017-04-27 12:56:33 -0700

    Added voxel object type

---
+++ include/VoxelObject.h
@@ -0,0 +1,18 @@
+#ifndef VOXEL_OBJECT_H
+#define VOXEL_OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+#include "object.h"
+
+class VoxelObject : public Object {
+  public:
+    VoxelObject();
+    ~VoxelObject();
+    void initialize();
+
+  private:
+
+};
+
+#endif /* VOXEL_OBJECT_H */
